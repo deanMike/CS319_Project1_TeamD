3cbd52848b87be1c7e8a32281b8d48c2ea6f83b9
Mike Dean <msd14@hampshire.edu>
2015-09-22 19:33:33 -0400

    'wasd' Movement and Mouse Rotation

---
+++ Assets/FlipScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        if (Input.GetKey("space")) {
+            transform.Translate(new Vector3(25, 0, 25));
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.transform.position = new Vector3(150, 15, 100);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey("w")) {
+            transform.Translate(0, 0, 3);
+        }
+        if (Input.GetKey("s")) {
+            transform.Translate(0, 0, -3);
+        }
+        if (Input.GetKey("a")) {
+            transform.Translate(-3, 0, 0);
+        }
+        if (Input.GetKey("d")) {
+            transform.Translate(3, 0, 0);
+        }
+	}
+}
