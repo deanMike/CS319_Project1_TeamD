4c3c70a6a7927aa5e43f26dcb9e6dcc58056b026
Mike Dean <msd14@hampshire.edu>
2015-10-04 16:32:22 -0400

    Audio Updates

---
+++ Assets/AudioManagerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManagerScript : MonoBehaviour {
+    public AudioClip[] audioClipArray;
+    public float[] volumeArray; 
+    private AudioSource music;
+    // Use this for initialization
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        
+    }
+
+	void Start () {
+            music = gameObject.GetComponent<AudioSource>();
+        if (Application.loadedLevelName == "TitleScreen")
+        {
+            music.clip = audioClipArray[0];
+            music.volume = volumeArray[0];
+            music.loop = true;
+            music.Play();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/ChairSpin.cs
+++ Assets/Scripts/ChairSpin.cs
@@ -5,10 +5,18 @@ using System;
 public class ChairSpin : MonoBehaviour {
     private bool isRotating = false;
     private float spinTime = 2.0f;
+    private bool soundPlay = false;
+
+    private AudioClip[] chairSounds;
+
+    void Start()
+    {
+    }
 
     void OnMouseDown()
     {
         Debug.Log(transform.rotation.y.ToString());
+        GetComponent<AudioSource>().Play();
         isRotating = true;
     }
 
@@ -23,7 +31,7 @@ public class ChairSpin : MonoBehaviour {
 
     void spinChair()
     {
-            transform.Rotate(Vector3.down, Time.deltaTime * 180, Space.World);
+        transform.Rotate(Vector3.down, Time.deltaTime * 180, Space.World);
         //Debug.Log(transform.rotation.y.ToString());
         if (spinTime <= 0)
         {
