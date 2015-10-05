2b7558e6b66ba80301610cd40eb73f6989dc9113
Mike Dean <msd14@hampshire.edu>
2015-10-05 10:10:58 -0400

    Fixed music issue.

--- Assets/TitleScreenScript.cs
+++ Assets/TitleScreenScript.cs
@@ -6,10 +6,16 @@ public class TitleScreenScript : MonoBehaviour {
     private GameObject Title;
     private GameObject StartButton;
     private GameObject MusicText;
-
+    public GameObject audioManager;
+    
 
 	// Use this for initialization
 	void Start () {
+        if (!GameObject.Find("AudioManager"))
+        {
+            Instantiate(audioManager);
+            GameObject.Find("AudioManager(Clone)").name = "AudioManager";
+        }
         MusicText = GameObject.Find("MusicText");
 	}
 	
